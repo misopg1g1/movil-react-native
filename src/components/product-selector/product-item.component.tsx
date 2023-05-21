37a4e094591270bcb8aa3ea8c9e55dde9a3a4d20
Daniel Larios <daniel.larios.torres@nui.care>
2023-05-21 15:42:03 +0200

    Se remueven console logs

--- src/components/product-selector/product-item.component.tsx
+++ src/components/product-selector/product-item.component.tsx
@@ -37,8 +37,6 @@ export default function ProductItem(props: ProductItemProps) {
       setCounter(counter - 1);
     }
   };
-
-  console.log(props.index, `plus-item-${props.index}`);
   return (
     <View style={styles.rowContainer}>
       <View style={styles.imageContainer}>
